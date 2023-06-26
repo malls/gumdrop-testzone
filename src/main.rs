d87c232276c0415569033c0b2b8cf59b51e98bb6
Forrest Almasi <forrest.almasi@gmail.com>
2023-06-26 17:05:56 -0400

    read local file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use tokio;
+use serde_json;
+
 use gumdrop::archive_getter;
 
 
+#[tokio::main]
+async fn main() {
 
-fn main() {
+    let msg = "{\"event_type\":\"mod\", \"file_path\":\"/Users/work/Desktop/file.txt\", \"context\":\"local\"}";
 
-    let msg = "{\"event_type\":\"mod\", \"file_path\":\"~/Desktop/file.txt\", \"context\":\"local\"}";
+    let msg2: archive_getter::Message = serde_json::from_str(msg).unwrap();
 
-    let res = archive_getter::get_file_contents(msg);
 
-    println!("{:?}", res);
+    let res = archive_getter::get_file_contents(msg2).await;
+
+    println!("{:?}", res.ok());
 
 }
