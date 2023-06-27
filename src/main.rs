08512d0855929fd3b06036a5d843e668ebee3e28
Forrest Almasi <forrest.almasi@gmail.com>
2023-06-27 11:23:54 -0400

    SB-738 update unwrapping

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use tokio;
 use serde_json;
 
-use gumdrop::archive_getter;
+use gumdrop::archive_getter::{Message, get_file_contents};
 
 
 #[tokio::main]
 async fn main() {
 
-    let msg = "{\"event_type\":\"mod\", \"file_path\":\"/Users/work/Desktop/file.txt\", \"context\":\"local\"}";
+    let msg: &str = "{\"event_type\":\"mod\", \"file_path\":\"/Users/work/Desktop/file.txt\", \"context\":\"local\"}";
 
-    let msg2: archive_getter::Message = serde_json::from_str(msg).unwrap();
+    let msg2: Message = serde_json::from_str(msg).unwrap();
+
+    let res = get_file_contents(msg2).await;
+    println!("local res: {:?}", res.ok().unwrap());
 
 
-    let res = archive_getter::get_file_contents(msg2).await;
+    let remote_msg: &str = "{\"event_type\":\"mod\", \"file_path\":\"https://c088-74-72-159-164.ngrok.io/file.txtxx\", \"context\":\"remote\"}";
+    let remote_msg2: Message = serde_json::from_str(remote_msg).unwrap();
 
-    println!("{:?}", res.ok());
+    let res = get_file_contents(remote_msg2).await;
+    println!("remote res: {:?}", res.ok().unwrap());
+
 
 }
