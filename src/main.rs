08f550d592887b4494f2d65259001657f6630f67
Forrest Almasi <forrest.almasi@gmail.com>
2023-06-30 11:38:38 -0400

    rename project, add tar

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use tokio;
 use serde_json;
 
-use gumdrop::archive_getter::{Message, get_file_contents};
+use gummiworm::archive_getter::{Message, get_file_contents};
 
 
 #[tokio::main]
@@ -12,14 +12,14 @@ async fn main() {
     let msg2: Message = serde_json::from_str(msg).unwrap();
 
     let res = get_file_contents(msg2).await;
-    println!("local res: {:?}", res.ok().unwrap());
+    println!("local res: {:?}", res);
 
 
-    let remote_msg: &str = "{\"event_type\":\"mod\", \"file_path\":\"https://c088-74-72-159-164.ngrok.io/file.txtxx\", \"context\":\"remote\"}";
+    let remote_msg: &str = "{\"event_type\":\"mod\", \"file_path\":\"https://c088-74-72-159-164.ngrok.io/file.txtxx\", \"context\":\"remoter\"}";
     let remote_msg2: Message = serde_json::from_str(remote_msg).unwrap();
 
     let res = get_file_contents(remote_msg2).await;
-    println!("remote res: {:?}", res.ok().unwrap());
+    println!("remote res: {:?}", res);
 
 
 }
